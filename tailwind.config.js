51aa270aa9da7f31fc551027a9437f3efe3f5222
Yoni van Haarlem <yoni.vh@gmail.com>
2022-05-12 13:02:54 +0200

    Buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,11 @@ module.exports = {
       },
       boxShadow: {
         'custom': '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 0 6px 0 rgb(0 0 0 / 0.05)',
+        'inner-primary': 'inset 0 0 0 1px #FF2D20',
+        'inner-light': 'inset 0 0 0 1px rgba(9, 9, 16, .2)',
+        'inner-outer-primary': 'inset 0 0 0 1px #FF2D20, 4px 4px 0 -1px #FFFFFF, 4px 4px 0 0 #FF2D20',
+        'outer-primary': '4px 4px 0 -1px #FFFFFF, 4px 4px 0 0 #FF2D20',
+        'outer-dark': '4px 4px 0 -1px #FFFFFF, 4px 4px 0 0 #000000',
       },
     },
     plugins: [
