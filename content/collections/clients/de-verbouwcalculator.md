4c9ac1f7bad6423d6c6190cb82a1c82d380ad49c
Yoni van Haarlem <yoni.vh@gmail.com>
2022-05-24 13:02:18 +0200

    Reviews for what is Laravel page

--- content/collections/clients/de-verbouwcalculator.md
+++ content/collections/clients/de-verbouwcalculator.md
@@ -3,8 +3,8 @@ id: 964d6872-f078-4702-ba76-de96e3123ba5
 blueprint: clients
 title: 'De Verbouwcalculator'
 updated_by: 8d873b47-ad86-4fd3-9e95-27842ea80beb
-updated_at: 1653312196
-contact: 'Lara Vel'
-contact_function: Webdesigner
-review: 'Werkt top!'
+updated_at: 1653382865
+review_text: 'Werkt top!'
+review_function: Webdesigner
+review_name: 'Lara Vel'
 ---
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,13 @@
-// This is all you.
+// import Swiper JS and it's styles
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const swiper = new Swiper('.swiper-carousel', {
+    modules: [ Navigation ],
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
