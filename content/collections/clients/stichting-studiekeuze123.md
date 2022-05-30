8a2c454ee667b6c1881de3b3da356223b7440b16
Yoni van Haarlem <yoni.vh@gmail.com>
2022-05-30 14:50:25 +0200

    Logo swiper WIP

--- content/collections/clients/stichting-studiekeuze123.md
+++ content/collections/clients/stichting-studiekeuze123.md
@@ -6,5 +6,6 @@ review_name: 'Gert-Jan Bos'
 review_function: 'Hoofd Communicatie'
 review_text: 'We zijn erg blij met de toepassing van Laravel voor ons platform Studiekeuze123.nl want Laravel biedt een solide basis voor een stabiel en veilig platform met maximale flexibiliteit om snel en efficiënt nieuwe onderdelen / functionaliteiten uit te rollen'
 updated_by: 8d873b47-ad86-4fd3-9e95-27842ea80beb
-updated_at: 1653382931
+updated_at: 1653912156
+logo: studiekeuze123-logo.png
 ---
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,13 +1,52 @@
 // import Swiper JS and it's styles
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Autoplay, Navigation } from 'swiper';
 import 'swiper/css';
+import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
 
-const swiper = new Swiper('.swiper-carousel', {
+// Carousel swiper (used on the What is Laravel page)
+new Swiper('.swiper-carousel', {
     modules: [ Navigation ],
     loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+});
+
+// Autoplay (logo) swiper (used on the homepage)
+function logoSwiperEnabled(carousel) {
+    const slidesCount = carousel.querySelectorAll('.swiper-slide').length;
+    let settings;
+    const wrapperWidth = carousel.querySelector('.js-swiper-wrapper').offsetWidth;
+    const slidesWidth = [...carousel.querySelectorAll('.swiper-slide')]
+        .map((el) => el.offsetWidth)
+        .reduce((total, width) => total + width);
+    if (slidesWidth < wrapperWidth) {
+        settings = {
+            loop: false,
+            slidesPerView: slidesCount,
+            enabled: false,
+        };
+        return settings;
+    }
+    return null;
+}
+
+new Swiper(`.swiper-autoplay`, {
+    modules: [ Autoplay ],
+    allowTouchMove: false,
+    slidesPerView: 10,
+    loop: true,
+    loopedSlides: 4,
+    freeMode: true,
+    spaceBetween: 60,
+    autoplay: {
+        delay: 2000,
+    },
+    speed: 2000,
+    freeModeMomentum: false,
+    preloadImages: true,
+    centeredSlides: true,
+    // ...logoSwiperEnabled(carousel)
 });
