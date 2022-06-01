faa66be95d5a77405d86656b7dca85aee7cffdcd
Yoni van Haarlem <yoni.vh@gmail.com>
2022-06-01 13:43:52 +0200

    Hide navigation when scrolling

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,16 +1,58 @@
+// Headroom.js
+import Headroom from "headroom.js";
+
+var header = document.querySelector('header'),
+    banner = document.querySelector('.banner');
+
+var includeBannerHeight = false;
+var bannerHeight = combinedHeaderHeight(includeBannerHeight);
+
+let isIE = /*@cc_on!@*/ false || !!document.documentMode;
+
+if (header != null && isIE === false) {
+    var headerHide = new Headroom(header, {
+        offset: 0,
+        tolerance: {
+            up: 5,
+            down: 0,
+        },
+        classes: {
+            initial: 'animated',
+            pinned: 'slideDown',
+            unpinned: 'slideUp',
+        },
+    });
+
+    headerHide.init();
+
+    // When the banner has changing heights else you can remove this part
+    window.addEventListener(
+        'load',
+        function () {
+            headerHide.offset = { down: combinedHeaderHeight(includeBannerHeight), up: combinedHeaderHeight(includeBannerHeight) };
+            console.log(headerHide.offset);
+        },
+        false,
+    );
+}
+
+function combinedHeaderHeight(includeBanner = true) {
+    return header.offsetHeight + (includeBanner && banner ? banner.offsetHeight: 0);
+}
+
 // SwiperJS
 import './components/swiper';
 
 // AlpineJS
-import Alpine from 'alpinejs'
-window.Alpine = Alpine
-Alpine.start()
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
 
 // Footprint
 if (!(window.console && window.console.log && window.console.group)) {
     window.console = {
-        group: function () {},
-        log: function () {}
+        group: function () { },
+        log: function () { }
     };
 }
 
