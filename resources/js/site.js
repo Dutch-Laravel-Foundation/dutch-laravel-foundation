7c1ca67cab39707e4c503256cc403176ba381648
Yoni van Haarlem <yoni.vh@gmail.com>
2022-06-01 10:51:48 +0200

    Add footprint

--- resources/js/site.js
+++ resources/js/site.js
@@ -5,3 +5,14 @@ import './components/swiper';
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
+
+// Footprint
+if (!(window.console && window.console.log && window.console.group)) {
+    window.console = {
+        group: function () {},
+        log: function () {}
+    };
+}
+
+window.console.group('DEVELOPER ALERT!');
+window.console.log('\nHallo daar! Wil jij ook zulke mooie dingen maken zoals deze website? Neem eens een kijkje bij ons op:\n\nwww.vormkracht10.nl\n\n');
